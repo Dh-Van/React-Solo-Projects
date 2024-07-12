5888c2da836016612ba23a77c168af278150914d
Dh-Van <dhvan17shah@gmail.com>
2024-07-11 21:26:25 -0400

    Finished About and Interests

--- Business-Card/src/components/About.jsx
+++ Business-Card/src/components/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default function About(){
+    const aboutInformation = "I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn."
     return (
-        <h1>About</h1>
+        <div className="about">
+            <h1>About</h1>
+            <p>{aboutInformation}</p>
+        </div>
     )
 }
--- Business-Card/src/components/Interests.jsx
+++ Business-Card/src/components/Interests.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 export default function Interests(){
+    const interestsInformation = "Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic."
     return (
-        <h1>Interests</h1>
+        <div className="interests">
+            <h1>Interests</h1>
+            <p>{interestsInformation}</p>
+        </div>
     )
 }
